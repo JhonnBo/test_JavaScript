4dbae9303ea5a77d5b50c4e509869b2276a314f7
Eugane <jhonnbo@gmail.com>
2020-06-23 18:05:04 +0300

    Мой первый коммит

---
+++ script.js
@@ -0,0 +1,25 @@
+//alert(1);
+
+//console.log(leftBorderWidth);
+const leftBorderWidth = 1;
+console.log('lalala' / 4);
+
+//console.log(fdsfsd);
+let a;
+console.log(a);
+
+let person = {
+    name: "Jhonn",
+    age: 25,
+    isMarried: false
+}
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.isMarried);
+
+console.log(person["name"]);
+
+let arr = ['plump.jpg', 'orange.jpg', 'apple'];
+
+console.log(arr[1]);
